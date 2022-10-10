450abd91d6ee31f24f421003c8c35a9d164ed604
FahadSaleem907 <52453895+FahadSaleem907@users.noreply.github.com>
2022-10-11 03:19:24 +0500

    First Push
    Fresh Project...
    Only has IQKeyboard pod (not setup yet)

---
+++ MultipleAuths/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultipleAuths
+//
+//  Created by Fahad Saleem on 11/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
